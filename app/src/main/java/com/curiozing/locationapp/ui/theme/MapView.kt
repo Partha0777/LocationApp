d20f12c36788143a626fbb7cad673f19360f1b40
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-03-11 23:23:45 +0530

    #Added map view.

--- app/src/main/java/com/curiozing/locationapp/ui/theme/MapView.kt
+++ app/src/main/java/com/curiozing/locationapp/ui/theme/MapView.kt
@@ -3,10 +3,12 @@ package com.curiozing.locationapp.ui.theme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.google.android.gms.maps.MapFragment
 
 @Composable
-fun mapView(){
+fun MapView(){
     Column {
         Text(text = "Map")
+        MapView()
     }
 }
